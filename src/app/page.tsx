67bb760cd4f7de22e67fea29e5b97f7efeab9f13
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2025-04-26 21:59:39 +0200

    feat(landing): add more content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,30 @@
-import { Calendar } from 'lucide-react';
+import { Separator } from '@/components/ui/separator';
+import { Calendar, Goal, LoaderCircle, Clipboard } from 'lucide-react';
+import Link from 'next/link';
+
+const indicators = [
+  {
+    icon: <Goal className='w-6 h-6 text-gray-700' />,
+    title: 'Na samochód',
+    value: 'PLN 45 350,00',
+    progress: '75%',
+    progressColor: 'text-yellow-600',
+  },
+  {
+    icon: <LoaderCircle className='w-6 h-6 text-gray-700' />,
+    title: 'Nauka TypeScript',
+    value: '14h',
+    progress: '60%',
+    progressColor: 'text-yellow-600',
+  },
+  {
+    icon: <Clipboard className='w-6 h-6 text-gray-700' />,
+    title: 'Oferty pracy',
+    value: '38 aplikacji',
+    progress: '',
+    progressColor: 'text-green-600',
+  },
+];
 
 export default function Home() {
   const today = new Date();
@@ -15,7 +41,7 @@ export default function Home() {
 
   return (
     <div className='min-h-screen px-5'>
-      <section className='flex justify-between'>
+      <section className='flex justify-between mb-4'>
         <div>
           <h1 className='font-semibold text-3xl mb-2 text-gray-900'>Cześć, Rafał</h1>
           <p className='text-sm text-gray-600 leading-relaxed'>
@@ -31,6 +57,40 @@ export default function Home() {
           </div>
         </div>
       </section>
+
+      <section>
+        <Separator className='mb-2' />
+        <div className='grid grid-cols-1 md:grid-cols-3'>
+          {indicators.map((indicator, index) => (
+            <div
+              key={index}
+              className='flex items-center p-4'
+            >
+              <div className='w-12 h-12 flex items-center justify-center bg-gray-200 rounded-full mr-4'>
+                {indicator.icon}
+              </div>
+              <div>
+                <Link href=''>
+                  <h2 className='text-sm font-semibold text-gray-800'>
+                    {indicator.title}
+                  </h2>
+                </Link>
+                <div className='flex items-baseline gap-2'>
+                  <span className='text-lg font-bold text-gray-900'>
+                    {indicator.value}
+                  </span>
+                  <span className={`text-xs ${indicator.progressColor}`}>
+                    {indicator.progress}
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Separator className='mt-2' />
+      </section>
+
+      <section></section>
     </div>
   );
 }
